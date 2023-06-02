87b42815fab586d208f1730c23ae10da0734922c
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-06-02 11:03:48 +0530

    os

--- osmodule.js
+++ osmodule.js
@@ -1,6 +1,13 @@
+
 const os = require('os');
 const path = require('path');
-console.log(os.freemem);
+console.log('freemem',os.freemem());
+console.log('homedir',os.homedir());
+console.log('hostname',os.hostname());
+console.log('platform',os.platform());
+console.log('release',os.release());
+console.log('totalmem',os.totalmem());
+
 const a1 = path.basename('C:\\temp\\myfile.html');
 const a2 = path.dirname('C:\\temp\\myfile.html');
 console.log(a1);
