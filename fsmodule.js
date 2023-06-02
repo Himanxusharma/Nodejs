3e2787b933d41c54285fd621e07b6670ccd3b67e
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-06-02 15:50:20 +0530

    os

---
+++ fsmodule.js
@@ -0,0 +1,25 @@
+// fs - file system
+
+const fs = require('fs');
+
+// fs.readFile('myFile.txt','utf8',(err,data)=>{
+//     console.log(err,data);
+// })
+
+// console.log('code completed');
+
+
+
+// const a = fs.readFileSync('myFile.txt');
+// console.log(a.toString());
+// console.log('code completed');
+
+
+// fs.writeFile('myFile.txt',"This the data written in the file.",()=>{
+//     console.log("Successfully wrtitten to the file.");
+// })
+
+
+
+// fs.writeFileSync('myFile.txt',"This the data written in the file by sync.");
+// console.log("Successfully wrtitten to the file.");
---
+++ moduleFirst.js
@@ -0,0 +1,3 @@
+import {simple2 as simple} from "./moduleSecond.mjs"
+// const simple2 = require(./moduleSecond.mjs);
+simple()
