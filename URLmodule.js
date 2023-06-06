3ec648398338d53eb99907d423e4992284f0720a
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-06-06 12:05:31 +0530

    event

--- URLmodule.js
+++ URLmodule.js
@@ -5,9 +5,9 @@ myURL.hash = '#fgh';
 
 console.log(myURL.href)
 
-const pathname = '/a/b/c';
-const search = '?d=e';
-const hash = '#fgh';
-const myURL = new URL(`https://example.org${pathname}${search}${hash}`); 
+// const pathname = '/a/b/c';
+// const search = '?d=e';
+// const hash = '#fgh';
+// const myURL = new URL(`https://example.org${pathname}${search}${hash}`); 
 
-console.log(myURL.href)
+// console.log(myURL.href)
